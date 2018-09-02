40fa8b85e7f5e815a351dddf857900137684c385
muzy06038 <sub06038@khu.ac.kr>
2018-09-02 22:20:25 +0900

    my first deeplearning 공부한거 추가

--- deeplearning/My_First_Deeplearning.py
+++ deeplearning/My_First_Deeplearning.py
@@ -28,7 +28,7 @@ model.add(Dense(1, activation='sigmoid'))
 
 # 딥러닝을 실행합니다.
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy']) # loss 한 번 신경망이 실행될 대마다 오차 값을 추적하는 함수. optimizer 오차를 어떻게 줄여 나갈지 정하는 함수.
-model.fit(X, Y, epochs=30, batch_size=10)
-
+model.fit(X, Y, epochs=30, batch_size=10)                                           # ㄴ metrics 함수 : 모델이 컴파일될 때 모델 수행 결과를 나타내게끔 설정하는 부분. 정확도를 측정하기 위해 사용되는 테스트 샘플을 학습 과정에서 제외시킴으로써 과적합 문제(특정 데이터에서는 잘 작동하나 다른 데이터에서는 잘 작동하지 않는 문제)를 방지하는 기능
+#epoch(에포크) : 전체 샘플 재사용이 30회가 될때 까지 반복    // batch_size : 숫자 만큼 끊어서 집어넣어
 # 결과를 출력합니다.
 print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
